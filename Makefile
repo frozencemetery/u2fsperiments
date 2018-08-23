86becdd012b9874f8f4ba137b6e72f2b7b6b145d
Robbie Harwood <rharwood@redhat.com>
2018-08-23 15:56:21 -0400

    Start client rewrite against libfido2

--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ LDFLAGS =
 u2fhost = $(shell pkg-config --cflags --libs u2f-host)
 u2fserver = $(shell pkg-config --cflags --libs u2f-server)
 
+fido2 = $(shell PKG_CONFIG_PATH=~/local/lib/pkgconfig pkg-config --cflags --libs libfido2)
+
 all: client server
 
 common.o: common.h common.c
@@ -15,7 +17,7 @@ base64.o: base64.h base64.c
 	$(CC) -o base64.o $(CFLAGS) -c base64.c
 
 client: client.c common.o
-	$(CC) $(CFLAGS) $(LDFLAGS) $(u2fhost) -o client client.c common.o
+	$(CC) $(CFLAGS) $(LDFLAGS) $(fido2) -o client client.c common.o
 
 server: server.c base64.o common.o
 	$(CC) $(CFLAGS) $(LDFLAGS) $(u2fserver) -o server server.c base64.o common.o
--- client.c
+++ client.c
@@ -3,124 +3,116 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <u2f-host.h>
+#include <fido.h>
+#include <fido/err.h>
 
 #include "common.h"
 
-u2fh_devs *setup_u2f() {
-    u2fh_devs *devs = NULL;
-    u2fh_rc ret;
-    unsigned num_devices;
+/* Why do I even have to specify this.  It's just calling malloc anyway. */
+#define MAX_DEVS 64
 
-    ret = u2fh_global_init(0); /* not enabling debug */
-    if (ret)
-        goto done;
+static const unsigned char challenge[32] = {
+    0xf9, 0x64, 0x57, 0xe7, 0x2d, 0x97, 0xf6, 0xbb,
+    0xdd, 0xd7, 0xfb, 0x06, 0x37, 0x62, 0xea, 0x26,
+    0x20, 0x44, 0x8e, 0x69, 0x7c, 0x03, 0xf2, 0x31,
+    0x2f, 0x99, 0xdc, 0xaf, 0x3e, 0x8a, 0x91, 0x6b,
+};
 
-    ret = u2fh_devs_init(&devs);
-    if (ret)
-        goto done;
-
-    /* this interface is bad */
-    ret = u2fh_devs_discover(devs, &num_devices);
-    num_devices++;
-    if (ret == U2FH_NO_U2F_DEVICE) {
-        fprintf(stderr, "No U2F devices found!\n");
-        ret = 0;
-        goto done;
-    } else if (ret)
-        goto done;
-
-    fprintf(stderr, "Detected %d device(s)\n", num_devices);
-
-    for (unsigned i = 0; i < num_devices; i++) {
-        size_t len = 78;
-        char buf[len];
-        ret = u2fh_get_device_description(devs, i, buf, &len);
-        if (ret)
-            goto done;
-
-        buf[len] = '\0';
-        fprintf(stderr, "Device %d: %s\n", i, buf);
-    }
-
-    return devs;
-
-done:
-    u2fh_devs_done(devs);
-    fprintf(stderr, "%s: %s\n", u2fh_strerror_name(ret), u2fh_strerror(ret));
-    return NULL;
-}
-
-static u2fh_rc sign_loop(u2fh_devs *devs) {
-    u2fh_rc ret;
-    char *challenge = NULL;
-    char response[MAX_REPLY_LEN];
-    size_t response_len = MAX_REPLY_LEN;
-
-    while (1) {
-        challenge = get();
-        if (!challenge || strlen(challenge) <= 1) {
-            printf("NO CHALLENGE\n");
-            free(challenge);
-            return 0;
-        }
-
-        fprintf(stderr, "PUSH BLINKY TO SIGN\n");
-
-        response_len = MAX_REPLY_LEN;
-        memset(response, 0, response_len);
-        ret = u2fh_authenticate2(devs, challenge, ORIGIN, response,
-                                 &response_len, U2FH_REQUEST_USER_PRESENCE);
-        free(challenge);
-        if (ret)
-            return ret;
-
-        response[response_len] = '\0';
-
-        put(response);
-    }
-}
-
-static u2fh_rc register_device(u2fh_devs *devs) {
-    u2fh_rc ret = 0;
-    char *challenge = NULL;
-    char response[MAX_REPLY_LEN];
-    size_t response_len = MAX_REPLY_LEN;
+static fido_dev_t *setup_u2f() {
+    fido_dev_info_t *devlist = NULL;
+    size_t num_devices = 0;
+    fido_dev_t *dev = NULL;
+    const char *path;
+    int ret = 0;
     
-    challenge = get();
+    fido_init(0);
 
-    fprintf(stderr, "PUSH BLINKY TO REGISTER\n");
-
-    ret = u2fh_register2(devs, challenge, ORIGIN, response, &response_len,
-                         U2FH_REQUEST_USER_PRESENCE);
-    if (ret)
+    devlist = fido_dev_info_new(MAX_DEVS);
+    if (!devlist)
         goto done;
-    response[response_len] = '\0';
 
-    put(response);
+    ret = fido_dev_info_manifest(devlist, MAX_DEVS, &num_devices);
+    if (ret != FIDO_OK || num_devices == 0) {
+        fprintf(stderr, "No U2F devices found!\n");
+        goto done;
+    }
+    fprintf(stderr, "Detected %ld device(s)\n", num_devices);
+
+    for (size_t i = 0; i < num_devices; i++) {
+        const fido_dev_info_t *di = fido_dev_info_ptr(devlist, i);
+        path = fido_dev_info_path(di);
+        fprintf(stderr, "Device %ld - %s; %ls; %ls\n", i, path,
+                fido_dev_info_manufacturer_string(di),
+                fido_dev_info_product_string(di));
+    }
+    if (num_devices > 1) {
+        fprintf(stderr, "More than one device found; I can't cope\n");
+        goto done;
+    }
+
+    dev = fido_dev_new();
+    if (dev == NULL)
+        goto done;
+
+    ret = fido_dev_open(dev, path);
+    if (ret != FIDO_OK) {
+        fido_dev_close(dev);
+        fido_dev_free(&dev);
+    }
 
 done:
-    free(challenge);
+    if (ret != FIDO_OK)
+        fprintf(stderr, "libfido2: %s\n", fido_strerr(ret));
+    fido_dev_info_free(&devlist, num_devices);
+    return dev;
+}
+
+static int chal(fido_dev_t *dev) {
+    fido_cred_t *cred = NULL;
+    int ret = 0;
+
+    cred = fido_cred_new();
+    if (cred == NULL)
+        goto done;
+
+    ret = fido_cred_set_type(cred, COSE_ES256);
+    if (ret != FIDO_OK)
+        goto done;
+    
+    ret = fido_cred_set_clientdata_hash(cred, challenge, sizeof(challenge));
+    if (ret != FIDO_OK)
+        goto done;
+
+    ret = fido_cred_set_rp(cred, ORIGIN, ORIGIN);
+    if (ret != FIDO_OK)
+        goto done;
+
+    fprintf(stderr, "PRESS BLINKY TO DO SOMETHING\n");
+
+    ret = fido_dev_make_cred(dev, cred, NULL);
+    if (ret != FIDO_OK)
+        goto done;
+
+done:
+    if (ret != FIDO_OK)
+        fprintf(stderr, "libfido2: %s\n", fido_strerr(ret));
+    fido_cred_free(&cred);
     return ret;
 }
 
 int main() {
-    u2fh_rc ret;
-    u2fh_devs *devs = NULL;
+    fido_dev_t *dev = NULL;
+    int ret = -1;
 
-    devs = setup_u2f();
-    if (!devs)
+    dev = setup_u2f();
+    if (!dev)
         goto done;
 
-    ret = register_device(devs);
+    ret = chal(dev);
     if (ret)
         goto done;
 
-    ret = sign_loop(devs);
-
+    ret = 0;
 done:
-    u2fh_devs_done(devs);
-    u2fh_global_done();
-
     return ret;
 }
